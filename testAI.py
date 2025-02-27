86ecdf7ae0e1fe3850ed88a85ad5435973ac3c92
sohail7777 <vsal30077@gmail.com>
2025-02-27 22:02:47 +0000

    test3

---
+++ testAI.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+
+
+bedrock_runtime = boto3.client('bedrock-runtime', region_name='us-west-2')
+
+prompt = "hello"
+
+
+
+kwargs = {
+    "modelId": "amazon.titan-text-lite-v1",
+    "contentType": "application/json",
+    "accept": "*/*",
+    "body": json.dumps(
+        {
+            "inputText": prompt
+        }
+    )
+}
+
+response = bedrock_runtime.invoke_model(**kwargs)
+
+response
+
+response_body = json.loads(response.get('body').read())
+
+#print(json.dumps(response_body, indent=4))
+
+print(response_body['results'][0]['outputText'])
+
